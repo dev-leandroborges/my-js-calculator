c247eed331b47bef4cc25f47db88aa932d3ea206
dev-leandroborges <devleandroborges>
2023-03-29 14:25:37 -0300

    Criada a função compute() para realizar os cálculos através da função eval().

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calcular (num) {
+function insert (num) {
     document.querySelector(".put").innerHTML += num;
 }
 
@@ -10,4 +10,13 @@ function backspace() {
     let visor = document.querySelector(".put").innerHTML;
     document.querySelector(".put").innerHTML = visor.substring(0, visor.length -1);
 
+}
+
+function compute() {
+    let visor = document.querySelector(".put").innerHTML;
+    if (visor) {
+        document.querySelector(".put").innerHTML = eval(visor);
+    }else {
+        document.querySelector(".put").innerHTML = "Nenhum valor";
+    }
 }
