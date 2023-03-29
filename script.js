f9480a2452a90109c78dcf25c1a608d30c195279
dev-leandroborges <devleandroborges>
2023-03-29 13:57:45 -0300

    Criada função para deletar números digitados, usando substring.

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ function calcular (num) {
 function clean() {
     document.querySelector(".put").innerHTML = "";
 }
+
+function backspace() {
+    let visor = document.querySelector(".put").innerHTML;
+    document.querySelector(".put").innerHTML = visor.substring(0, visor.length -1);
+
+}
